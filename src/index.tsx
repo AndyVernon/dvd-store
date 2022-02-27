2f10ba5613d502ee72c171ce637dfa24cc63f578
andyvernon <andy_vernon@live.co.uk>
2022-02-27 16:16:09 +0000

    Renamed app

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import DVDStore from './DVDStore';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DVDStore />
   </React.StrictMode>,
   document.getElementById('root')
 );
